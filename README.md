2d198025df55c72ba41728e77f8f041f809650c5
CMFerrer <70974749+CMFerrer@users.noreply.github.com>
2023-09-09 21:44:07 -0500

    Update Readme

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# SolarIconSetAndroid
- 
+# Solar Icon Set Android
+
+Librería para Android que provee íconos de [Solar Icon Set](https://solariconset.com/)  de [480-Design](https://github.com/480-Design)
+## Implementación
+
+Disponible en `mavenCentral()`.
+
+```kotlin
+implementation("com.chiksmedina.solar:solar:1.0.0")
+```
+## Uso
+
+```kotlin
+Icon(imageVector = Solar.Linear.Arrows.ArrowUp, contentDescription = "Solar icon")
+
+```
+
+
+## Gracias a
+
+- [@480-Design](https://github.com/480-Design) por el permiso de utilizar sus íconos en esta librería
+- [@overpas](https://github.com/overpas/svg-to-compose-intellij)
+- [@DevSrSouza](https://github.com/DevSrSouza/svg-to-compose)
--- sample/src/main/java/com/chiksmedina/sample/MainActivity.kt
+++ sample/src/main/java/com/chiksmedina/sample/MainActivity.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.AddCircle
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,11 +35,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    /*Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )*/
-    Icon(Solar.Linear.Arrows.ArrowUp, "Hello $name!", modifier = modifier)
+    Icon(imageVector = Solar.Linear.Arrows.ArrowUp, contentDescription = "Hello $name!", modifier = modifier)
 }
 
 @Preview(showBackground = true)
